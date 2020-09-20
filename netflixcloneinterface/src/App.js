eb30fa47a8c05db2dcba034d3aad6a256841d973
Thiago Marques Siqueira <thiago@pop-os.localdomain>
2020-09-20 15:20:25 +0100

    Implemented the first version:  - Get the info from API  - Create a list with this info

--- netflixcloneinterface/src/App.js
+++ netflixcloneinterface/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]) ;
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      // Get the list of contents
+      let list = await Tmdb.getHomeList();
+
+      console.log(list);
+      setMovieList(list);
+
+    }
+
+    loadAll();
+  }, []);
+
   return(
-    <div>
-      Hello World
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
